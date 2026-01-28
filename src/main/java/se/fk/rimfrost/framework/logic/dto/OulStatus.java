3861bb66133c827b52ccc32ab38faeac607c9d2a
Ulf Slunga <ulf.slunga@sinetiq.se>
2026-01-28 12:16:00 +0100

    Lägger till utforarId i OUL status

--- src/main/java/se/fk/rimfrost/framework/logic/dto/OulStatus.java
+++ src/main/java/se/fk/rimfrost/framework/logic/dto/OulStatus.java
@@ -10,6 +10,8 @@ public interface OulStatus
 
    UUID uppgiftId();
 
+   UUID utforarId();
+
    UppgiftStatus uppgiftStatus();
 
 }
--- src/main/java/se/fk/rimfrost/framework/presentation/kafka/OulKafkaMapper.java
+++ src/main/java/se/fk/rimfrost/framework/presentation/kafka/OulKafkaMapper.java
@@ -5,42 +5,39 @@ import se.fk.rimfrost.OperativtUppgiftslagerResponseMessage;
 import se.fk.rimfrost.OperativtUppgiftslagerStatusMessage;
 import se.fk.rimfrost.Status;
 import se.fk.rimfrost.framework.logic.dto.*;
+
 import java.util.UUID;
 
 @ApplicationScoped
-public class OulKafkaMapper
-{
+public class OulKafkaMapper {
 
-   public OulResponse toOulResponse(OperativtUppgiftslagerResponseMessage oulResponseMessage)
-   {
-      return ImmutableOulResponse.builder()
-            .kundbehovsflodeId(UUID.fromString(oulResponseMessage.getKundbehovsflodeId()))
-            .uppgiftId(UUID.fromString(oulResponseMessage.getUppgiftId()))
-            .build();
-   }
+    public OulResponse toOulResponse(OperativtUppgiftslagerResponseMessage oulResponseMessage) {
+        return ImmutableOulResponse.builder()
+                .kundbehovsflodeId(UUID.fromString(oulResponseMessage.getKundbehovsflodeId()))
+                .uppgiftId(UUID.fromString(oulResponseMessage.getUppgiftId()))
+                .build();
+    }
 
-   public OulStatus toOulStatus(OperativtUppgiftslagerStatusMessage oulStatusMessage)
-   {
+    public OulStatus toOulStatus(OperativtUppgiftslagerStatusMessage oulStatusMessage) {
 
-      return ImmutableOulStatus.builder()
-            .kundbehovsflodeId(UUID.fromString(oulStatusMessage.getKundbehovsflodeId()))
-            .uppgiftId(UUID.fromString(oulStatusMessage.getUppgiftId()))
-            .uppgiftStatus(mapStatus(oulStatusMessage.getStatus()))
-            .build();
-   }
+        return ImmutableOulStatus.builder()
+                .kundbehovsflodeId(UUID.fromString(oulStatusMessage.getKundbehovsflodeId()))
+                .uppgiftId(UUID.fromString(oulStatusMessage.getUppgiftId()))
+                .utforarId(UUID.fromString(oulStatusMessage.getUtforarId()))
+                .uppgiftStatus(mapStatus(oulStatusMessage.getStatus()))
+                .build();
+    }
 
-   private UppgiftStatus mapStatus(Status status)
-   {
+    private UppgiftStatus mapStatus(Status status) {
 
-      switch (status)
-      {
-         case NY:
-            return UppgiftStatus.NY;
-         case TILLDELAD:
-            return UppgiftStatus.TILLDELAD;
-         case AVSLUTAD:
-         default:
-            return UppgiftStatus.AVSLUTAD;
-      }
-   }
+        switch (status) {
+            case NY:
+                return UppgiftStatus.NY;
+            case TILLDELAD:
+                return UppgiftStatus.TILLDELAD;
+            case AVSLUTAD:
+            default:
+                return UppgiftStatus.AVSLUTAD;
+        }
+    }
 }
